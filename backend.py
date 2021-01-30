96935a7fe0925cff07af2918ff0dacc681e4952e
Jacqueline1201 <57199142+Jacqueline1201@users.noreply.github.com>
2021-01-30 10:13:23 -0800

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,12 @@
+
+'''
+pls ignore this im dying 
+from flask import flask
+app = Flask(__main__)
+
+def home():
+    return "Hello! this is the main page <h1>HELLO<h1>"
+
+if __name__== "__main__":
+        app.run()
+        '''
